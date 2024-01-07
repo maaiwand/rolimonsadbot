6a48312ccabd289437de0767c3bfb47da4d04d92
maiwand <72661293+maaiwand@users.noreply.github.com>
2024-01-07 12:26:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ general={
 }
 users=[
     {
-        Roli_cookie: process.env.login,
-        UserID: 3450002500,
+        Roli_cookie: process.env.roli,
+        UserID: ID,
         RoliAd: {
             item_ids: [987022351], 
             r_items: [], 
