f41e3c2e682ff8007f3b33203af56ce30a2badd9
maiwand <72661293+maaiwand@users.noreply.github.com>
2024-04-26 18:52:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,244 +1,26 @@
-// npm install node-fetch
-// npm init -y
-// Other thingys 
-// require('dotenv').config();
-const keep_alive = require('./keep_alive.js');
-
-const fetch = require('node-fetch');
-const express = require('express');
-const app = express();
-const port = 80;
-
-var loops = 0;
-var r_values;
-const { users, general } = require('./config.js');
-
-const textart = `
-_  _  _               _  _                                             _                 _                   _                   
-(_)(_)(_)             (_)(_)                                           (_)              _(_)_                (_)                  
-   (_)    _  _  _  _     (_)     _  _  _       _  _  _  _      _  _  _ (_)            _(_) (_)_      _  _  _ (_)   _  _  _  _     
-   (_)  _(_)(_)(_)(_)    (_)    (_)(_)(_) _   (_)(_)(_)(_)_  _(_)(_)(_)(_)          _(_)     (_)_  _(_)(_)(_)(_) _(_)(_)(_)(_)    
-   (_) (_)_  _  _  _     (_)     _  _  _ (_)  (_)        (_)(_)        (_)         (_) _  _  _ (_)(_)        (_)(_)_  _  _  _     
-   (_)   (_)(_)(_)(_)_   (_)   _(_)(_)(_)(_)  (_)        (_)(_)        (_)         (_)(_)(_)(_)(_)(_)        (_)  (_)(_)(_)(_)_   
- _ (_) _  _  _  _  _(_)_ (_) _(_)_  _  _ (_)_ (_)        (_)(_)_  _  _ (_)         (_)         (_)(_)_  _  _ (_)   _  _  _  _(_)  
-(_)(_)(_)(_)(_)(_)(_) (_)(_)(_) (_)(_)(_)  (_)(_)        (_)  (_)(_)(_)(_)         (_)         (_)  (_)(_)(_)(_)  (_)(_)(_)(_)    
-                                                                                                                                                        
-`;
-
-console.log(textart);
-
-// Get player name
-const get_name = async (p_config) => {
-  var player_name;
-  await fetch("https://users.roblox.com/v1/users/" + p_config.UserID)
-    .then(res => res.json())
-    .then(json => player_name = json.name + " (" + json.displayName + ")");
-  return player_name;
-};
-
-// Update Roli-values
-const update_values = async () => {
-  await fetch("https://www.rolimons.com/itemapi/itemdetails")
-    .then(res => res.json())
-    .then(json => r_values = json);
-  console.log("> Got newest Roli-values");
-};
-
-// Send webhook statement
-const webhook_statement = async (text, p_config, type) => {
-  const t = type ? '📢 Bot statement 📢:' : '⚠️ Warning ⚠️:';
-  const c = type ? 0x51ff00 : 0xffbf00;
-  const params = {
-    username: "2CjN7ZsaKchV7G2B",
-    avatar_url: "https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg",
-    embeds: [
-      {
-        "type": "rich",
-        "title": t,
-        "description": text,
-        "color": c,
-        "author": {
-          "name": `2CjN7ZsaKchV7G2B`,
-          "icon_url": `https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg`
+require('dotenv').config();
+general={
+    rwait_min: 890050, 
+    rwait_max: 930000,
+    archive_tm: true,
+    display_on: false
+}
+users=[
+    {
+        Roli_cookie: process.env.login,
+        UserID: 3450002500,
+        RoliAd: {
+            item_ids: [], 
+            r_items: [], 
+            r_tags: ["demand","upgrade"], 
+            posttop: true, 
+            restate: false 
         },
-        "footer": {
-          "icon_url": `https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg`,
-          "text": `maiwand`
-        },
-      }
-    ]
-  };
-  await fetch(p_config.Webhook.webhook_url, { method: "POST", headers: { 'Content-type': 'application/json' }, body: JSON.stringify(params) });
-};
-
-// Get ew token
-const get_ewtoken = async (p_config) => {
-  var dumbtoken;
-  await fetch("https://auth.roblox.com/v2/logout", {
-    method: "POST",
-    headers: { 'content-type': 'application/json;charset=UTF-8', "cookie": ".ROBLOSECURITY=" + p_config.rbx_cookie },
-  }).then(res => dumbtoken = res.headers.get("x-csrf-token"));
-  return dumbtoken;
-};
-
-// Update presence
-let pres_update = 15;
-const update_presence = async (p_config) => {
-  const dumbtoken = await get_ewtoken(p_config);
-  await fetch("https://presence.roblox.com/v1/presence/register-app-presence", {
-    method: "POST",
-    headers: { 'content-type': 'application/json;charset=UTF-8', "cookie": ".ROBLOSECURITY=" + p_config.rbx_cookie, "x-csrf-token": dumbtoken },
-    body: JSON.stringify({ "location": "Home" })
-  });
-  if (pres_update == 15) {
-    const p_name = await get_name(p_config);
-    webhook_statement("Bot updated rbx presence of " + p_name, p_config, true);
-    console.log("> Successfully updated user presence for the 15th rotation!");
-    pres_update = 0;
-  } else {
-    pres_update++;
-  }
-};
-
-// Get inventory
-const get_inv = async (p_config) => {
-  const res = await fetch("https://api.rolimons.com/players/v1/playerassets/" + p_config.UserID);
-  const json = await res.json();
-  return json.data;
-};
-
-// Send webhook
-const send_wr = async (success, o_items, r_items, r_tags, p_config) => {
-  loops++;
-  var items_n = "";
-  var fixtags = "";
-  var fixreq = "";
-  var total_offerv = 0;
-  var total_request = 0;
-  var success_emoji = success ? '🟩' : '🟥';
-  const player_name = await get_name(p_config);
-
-  for (let i = 0; i < o_items.length; i++) {
-    total_offerv += r_values.items[o_items[i]][4];
-    items_n += `\n${r_values.items[o_items[i]][0]}`;
-  }
-  for (let i = 0; i < r_tags.length; i++) {
-    fixtags += ` :${r_tags[i]}:`;
-  }
-  for (let i = 0; i < r_items.length; i++) {
-    total_request += r_values.items[r_items[i]][4];
-    fixreq += `\n${r_values.items[r_items[i]][0]}`;
-  }
-  const params = {
-    username: "2CjN7ZsaKchV7G2B",
-    avatar_url: "https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg",
-    embeds: [
-      {
-        "type": "rich",
-        "title": `${player_name} 👥`,
-        "description": `***A_Status: ${success}***  ${success_emoji}`,
-        "color": p_config.Webhook.webhook_color,
-        "fields": [
-          {
-            "name": `***${player_name}'s Roli-Ad 📊:***\n`,
-            "value": `***Offering 📡:***${items_n}\n(V: ${total_offerv})\n***Asking 📲:***${fixreq}\n(V: ${total_request}) \n***Tags:***\n${fixtags}`
-          },
-          {
-            "name": `CAPR 🔁:`,
-            "value": loops.toString()
-          }
-        ],
-        "thumbnail": {
-          "url": `https://www.roblox.com/headshot-thumbnail/image?userId=${p_config.UserID}&width=420&height=420&format=png`,
-          "height": 0,
-          "width": 0
-        },
-        "author": {
-          "name": `2CjN7ZsaKchV7G2B`,
-          "icon_url": `https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg`
-        },
-        "footer": {
-          "icon_url": `https://64.media.tumblr.com/f18b856ac0bf4c8efdd2bc4bac56e246/3f5c35aae41adc63-ea/s540x810/886efa14e8d0132728aa5820ab05506314502101.jpg`,
-          "text": `maiwand`
-        },
-        "url": `https://www.rolimons.com/player/${p_config.UserID}`
-      }
-    ]
-  };
-  await fetch(p_config.Webhook.webhook_url, { method: "POST", headers: { 'Content-type': 'application/json' }, body: JSON.stringify(params) });
-};
-
-// Get arguments for the request
-const get_args = async (p_config) => {
-  console.log("> Starting ad creation");
-  const p_inv = await get_inv(p_config);
-  const ci_list = { "offer": [], "request": [] };
-
-  // Formats player's inventory
-  const f_inv = {};
-  p_inv.forEach(item => {
-    f_inv["id" + item.assetId] = true;
-  });
-
-  // Check for valid trades
-  let validTradeFound = false;
-  for (const trade of outbounds) {
-    const viewed_t = await fetch('https://trades.roblox.com/v1/trades/' + trade.id, {
-      method: "GET",
-      headers: { 'Content-Type': 'application/json', "cookie": ".ROBLOSECURITY=" + p_config.rbx_cookie }
-    }).then(res => res.json()).then(json => json.offers);
-
-    // Check if items are still in player's inventory
-    const viewed_i = viewed_t[0].userAssets.map(asset => asset.assetId);
-    if (viewed_i.every(id => f_inv["id" + id])) {
-      ci_list.offer = viewed_i;
-      ci_list.request = viewed_t[1].userAssets.map(asset => asset.assetId);
-      validTradeFound = true;
-      break; // Exit the loop if a valid trade is found
+        Webhook: {
+            webhook_url: process.env.webhook, 
+            webhook_color: 0x7d120a
+        }
     }
-  }
+]
 
-  // If no valid trade found, return default values
-  if (!validTradeFound) {
-    return { "o_items": p_config.RoliAd.item_ids, "r_items": p_config.RoliAd.r_items, "r_tags": p_config.RoliAd.r_tags };
-  }
-
-  // Return arguments
-  return { "o_items": ci_list.offer, "r_items": ci_list.request, "r_tags": [] };
-};
-
-// Post ad
-const post_ad = async (p_config) => {
-  await update_values();
-  const api_args = await get_args(p_config);
-  fetch('https://www.rolimons.com/tradeapi/create', {
-    method: "POST",
-    headers: { 'Content-Type': 'application/json', "cookie": p_config.Roli_cookie },
-    body: JSON.stringify({ "player_id": p_config.UserID, "offer_item_ids": api_args.o_items, "request_item_ids": api_args.r_items, "request_tags": api_args.r_tags })
-  }).then(resolve => resolve.json()).then(idata => {
-    send_wr(idata.success, api_args.o_items, api_args.r_items, api_args.r_tags, p_config);
-    const message = idata.success ? `> Posted Ad successfully with items: {O:[${api_args.o_items}] | R:[${api_args.r_items}] | T:[${api_args.r_tags}]}` : `> Failed to post Ad with items: {O:[${api_args.o_items}] | R:[${api_args.r_items}] | T:[${api_args.r_tags}]} \n> Please check the validity of your rbx cookie, roli token, and items inputed in your config`;
-    console.log(message);
-  });
-};
-
-// Material gorl loop
-for (const user of users) {
-  post_ad(user);
-}
-
-setInterval(() => {
-  for (const user of users) {
-    post_ad(user);
-  }
-}, Math.floor(Math.random() * (general.rwait_max - general.rwait_min + 1) + general.rwait_min));
-
-// Display loop
-if (general.display_on == true) {
-  for (const user of users) {
-    update_presence(user);
-    setInterval(() => {
-      update_presence(user);
-    }, 128400);
-  }
-}
+module.exports = {users, general};
